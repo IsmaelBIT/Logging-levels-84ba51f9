2d27f0df6446acea3a36c2508cbef96cd2d52b01
IsmaelKleintjes <45165373+IsmaelKleintjes@users.noreply.github.com>
2020-04-14 14:12:27 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -5,7 +5,19 @@ class MyLogger
     {
         switch ($type) {
             case 'INFO':
-                echo 'INFO: ' . $message . "\n";
+                $this->info($message);
+                break;
+            case 'ERROR':
+                $this->error($message);
+                break;
+            case 'WARNING':
+                $this->warning($message);
+                break;
+            case 'DEBUG':
+                $this->debug($message);
+                break;
+            default:
+                echo $message;
                 break;
         }
     }
@@ -14,7 +26,22 @@ class MyLogger
     {
         echo 'ERROR: ' . $message . "\n";
     }
+
+    public function info($message) {
+        echo 'INFO: ' . $message . "\n";
+    }
+
+    public function warning($message)
+    {
+        echo 'WARNING: ' . $message . "\n";
+    }
+
+    public function debug($message) {
+        echo 'DEBUG: ' . $message . "\n";
+    }
+
 }
+
 
 $logger = new MyLogger();
 $logger->log('Hey! :)', 'INFO');
